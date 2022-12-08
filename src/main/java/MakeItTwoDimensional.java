979172c581dd520af01a4dbfa49da8016ef9e463
Seowoo <5w31892p@gmail.com>
2022-12-08 11:11:23 +0900

    2차원으로 만들기

---
+++ src/main/java/MakeItTwoDimensional.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MakeItTwoDimensional {
+    public int[][] solution(int[] num_list, int n) {
+            int[][] answer = new int[num_list.length / n][n];
+            int num = 0;
+            for (int i = 0; i < num_list.length; i++) {
+                for (int j; j < n; j++) {
+                    answer[i][j] = num_list[num];
+                    num++;
+                }
+            }
+        return answer;
+    }
+}
+
+
+//import java.util.Arrays;
+//
+//    public int[][] solution(int[] num_list, int n) {
+//        int[][] answer = {};
+//        for (int i = 0; i < num_list.length; i += n) {
+//            System.out.println(Arrays.toString(Arrays.copyOfRange(num_list, i, Math.min(num_list.length, i + n))));
+//        }
+//        return answer;
+//    }
+
+
