628bb10925f67460532357f22aaac1a2eda085a5
Seowoo <5w31892p@gmail.com>
2022-12-09 16:07:35 +0900

    필요없는 import delete

--- src/main/java/MakeItTwoDimensional.java
+++ src/main/java/MakeItTwoDimensional.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MakeItTwoDimensional {
     public int[][] solution(int[] num_list, int n) {
             int[][] answer = new int[num_list.length / n][n];
             int num = 0;
             for (int i = 0; i < num_list.length; i++) {
-                for (int j; j < n; j++) {
+                for (int j = 0; j < n; j++) {
                     answer[i][j] = num_list[num];
                     num++;
                 }
